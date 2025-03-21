86ef7a95687f66543f62d7b84aa23220ff3a7ff6
Anthony Capobianchi <anthony.capobianchi@gmail.com>
2025-03-21 10:40:26 -0400

    started adding ufo system:

---
+++ src/game/components/ufoSpawner.ts
@@ -0,0 +1,5 @@
+import { defineComponent, Types } from "bitecs";
+
+const UFOSpawner = defineComponent( {
+  interval : Types.f32
+} )
---
+++ src/game/systems/ufoSpawnSystem.ts
@@ -0,0 +1,12 @@
+import { defineSystem } from "bitecs";
+import type { IWorld } from "bitecs";
+
+export default function UFOSpawnSystem()
+{
+  return defineSystem( ( _world : IWorld ) => {
+
+
+
+    return _world;
+  } );
+}
